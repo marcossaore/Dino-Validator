ebd61f215c57135ddfebad5357dd3c96f9080272
Marcos Soares <marcossaore@gmail.com>
2021-07-31 21:51:00 -0300

    chore: format jest config

--- jest.config.js
+++ jest.config.js
@@ -1,19 +1,18 @@
 module.exports = {
-    roots: ['<rootDir>/tests'],
-    collectCoverageFrom: [
-      '<rootDir>/src/**/*.ts',
-      '!<rootDir>/src/main/**'
-    ],
-    coverageDirectory: 'coverage',
-    coverageProvider: 'babel',
-    testEnvironment: 'node',
-    transform: {
-      '.+\\.ts$': 'ts-jest'
-    },
-    moduleNameMapper: {
-      '@/tests/(.*)': '<rootDir>/tests/$1',
-      '@/(.*)': '<rootDir>/src/$1'
-    },
-    testMatch: ['**/*.spec.ts']
-  }
-  
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/tests/(.*)': '<rootDir>/tests/$1',
+    '@/(.*)': '<rootDir>/src/$1'
+  },
+  testMatch: ['**/*.spec.ts']
+}
