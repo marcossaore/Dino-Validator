54e64a65b4816fa49378c45773e78991acb9dc3b
Marcos Soares <marcossaore@gmail.com>
2021-07-31 21:55:55 -0300

    feat: add dino-validator features

---
+++ src/main/dino-validator.ts
@@ -0,0 +1,11 @@
+import { Validation } from '@/presentation/protocols/validation'
+import { ValidationFacade } from '@/validation/facade/validation-facade'
+import { ValidationModel } from '@/validation/facade/validation-facade-types'
+
+export class DinoValidator implements Validation {
+  constructor (private readonly validationModel: ValidationModel) {}
+
+  validate (input: any): Error {
+    return (new ValidationFacade(this.validationModel)).validate(input)
+  }
+}
